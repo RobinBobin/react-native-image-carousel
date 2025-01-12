fc31f0fad79dfae2f8feaab107cc76043718dcbd
Ruben Shalimov <r_shalimov@inbox.ru>
2025-01-12 16:47:54 +0300

    imports fixed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { setHandleError } from './handleError'
 export { ImageCarousel } from './ImageCarousel'
 export { useImageCarouselModel } from './ImageCarousel/hooks/useImageCarouselModel'
 export type { TUseImageCarouselReturnType } from './ImageCarousel/hooks/useImageCarouselModel/types'
